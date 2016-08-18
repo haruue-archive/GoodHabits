cfcdff115a67fbae02e36d38ffc6d92f070bda01
simonla <zengmingyang96@gmail.com>
2016-08-18 15:07:30 +0800

    toolbar background

--- app/src/main/java/moe/haruue/goodhabits/ui/activity/LauncherActivity.java
+++ app/src/main/java/moe/haruue/goodhabits/ui/activity/LauncherActivity.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 
 import moe.haruue.goodhabits.data.FirstTime;
 import moe.haruue.goodhabits.guide.GuideActivity;
+import moe.haruue.goodhabits.login.LoginActivity;
 import moe.haruue.goodhabits.model.CurrentUser;
 
 /**
  * 启动 Activity ，用于判断启动时显示哪一个页面，不应该在这个 Activity 里显示任何东西
+ *
  * @author Haruue Icymoon haruue@caoyue.com.cn
  */
 public class LauncherActivity extends BaseActivity {
@@ -18,7 +20,7 @@ public class LauncherActivity extends BaseActivity {
         if (FirstTime.isFirstTimeStart()) {
             GuideActivity.start(this);
         } else if (!CurrentUser.getInstance().isLogin()) {
-
+            LoginActivity.start(this);
         } else {
             MainActivity.start(this);
         }
