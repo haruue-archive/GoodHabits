a69316ff86112e55e4e677fcb9cc870922c6371d
Haruue Icymoon <haruue@caoyue.com.cn>
2016-08-18 21:17:23 +0800

    delete useless method

--- app/src/main/java/moe/haruue/goodhabits/util/BitmapUtils.java
+++ app/src/main/java/moe/haruue/goodhabits/util/BitmapUtils.java
@@ -2,39 +2,12 @@ package moe.haruue.goodhabits.util;
 
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
-import android.util.Log;
 
 /**
  * @author Haruue Icymoon haruue@caoyue.com.cn
  */
 
 public class BitmapUtils {
-
-    /**
-     * 根据给定的宽和高进行拉伸
-     *
-     * @param origin    原图
-     * @param newWidth  新图的宽
-     * @param newHeight 新图的高
-     * @return new Bitmap
-     */
-    public static Bitmap scaleBitmap(Bitmap origin, int newWidth, int newHeight) {
-        Log.d("BitmapClipper", "w=" + newWidth + "h=" + newHeight);
-        if (origin == null) {
-            return null;
-        }
-        int height = origin.getHeight();
-        int width = origin.getWidth();
-        float scaleWidth = ((float) newWidth) / width;
-        float scaleHeight = ((float) newHeight) / height;
-        Matrix matrix = new Matrix();
-        matrix.postScale(scaleWidth, scaleHeight);// 使用后乘
-        Bitmap newBM = Bitmap.createBitmap(origin, 0, 0, width, height, matrix, false);
-        if (!origin.isRecycled()) {
-            origin.recycle();
-        }
-        return newBM;
-    }
 
     public static Bitmap zoomImg(Bitmap bm, int newWidth ,int newHeight){
         // 获得图片的宽高
