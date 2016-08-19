2324a272164d4b4ae78950d776c5d392a693da61
Haruue Icymoon <haruue@caoyue.com.cn>
2016-08-20 00:00:08 +0800

    TaskContract 实现于 TaskPresenter

--- app/src/main/java/moe/haruue/goodhabits/ui/task/TaskPresenter.java
+++ app/src/main/java/moe/haruue/goodhabits/ui/task/TaskPresenter.java
@@ -1,5 +1,18 @@
 package moe.haruue.goodhabits.ui.task;
 
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import moe.haruue.goodhabits.data.database.func.TasksByIdQueryFunc;
+import moe.haruue.goodhabits.data.database.func.TasksByTimeQueryFunc;
+import moe.haruue.goodhabits.model.Task;
+import moe.haruue.goodhabits.util.TimeUtils;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
 /**
  * Created by simonla on 2016/8/18.
  * Have a good day.
@@ -20,11 +33,53 @@ public class TaskPresenter implements TaskContract.Presenter {
 
     @Override
     public void getTodayTasks() {
+        long todayStartTimeStamp = TimeUtils.timeStampToDayStartCCT(new GregorianCalendar().getTimeInMillis() / 1000);
+        Observable.just(Task.newEmptyTaskWithStartTimeAndEndTime(todayStartTimeStamp, todayStartTimeStamp + 86400))
+                .map(new TasksByTimeQueryFunc())
+                .subscribeOn(Schedulers.io())
+                .unsubscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<List<Task>>() {
+                    @Override
+                    public void onCompleted() {
 
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        mView.onGetTodayTasks(null, false);
+                    }
+
+                    @Override
+                    public void onNext(List<Task> tasks) {
+                        mView.onGetTodayTasks(new ArrayList<>(tasks), true);
+                    }
+                });
     }
 
     @Override
     public void setTaskFinish(int TaskId) {
+        Observable.just(Task.newEmptyTaskWithId(TaskId))
+                .map(new TasksByIdQueryFunc())
+                .subscribeOn(Schedulers.io())
+                .unsubscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<List<Task>>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        mView.onSetTaskFinished(false);
+                    }
+
+                    @Override
+                    public void onNext(List<Task> tasks) {
+                        mView.onSetTaskFinished(true);
+                    }
+                });
 
     }
 }
---
+++ app/src/main/java/moe/haruue/goodhabits/util/TimeUtils.java
@@ -0,0 +1,32 @@
+package moe.haruue.goodhabits.util;
+
+import java.util.Calendar;
+
+/**
+ * @author Haruue Icymoon haruue@caoyue.com.cn
+ */
+
+public class TimeUtils {
+
+    /**
+     * 取北京时间当天 0 点的时间戳
+     * @param timeStamp 北京时间某一天任意时间的时间戳
+     * @return 当天 0 点的时间戳
+     */
+    public static long timeStampToDayStartCCT(long timeStamp) {
+        timeStamp += 28800;
+        timeStamp = timeStamp / 86400 * 86400;
+        return timeStamp - 28800;
+    }
+
+    /**
+     * 取以秒为单位的时间戳<br>
+     *     <del>让我用以毫秒为单位的时间戳我是拒绝的</del>
+     * @param calendar 需要取值的 {@link Calendar} 对象
+     * @return 以秒为单位的时间戳
+     */
+    public static long getTimeStampOf(Calendar calendar) {
+        return calendar.getTimeInMillis() / 1000;
+    }
+
+}
