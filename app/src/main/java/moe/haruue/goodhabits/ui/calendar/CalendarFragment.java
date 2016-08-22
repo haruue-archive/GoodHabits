832c877def76d1b11d45bc0f467ed1c9bb34b184
simonla <zengmingyang96@gmail.com>
2016-08-22 21:22:10 +0800

    add a calendar

--- app/src/main/java/moe/haruue/goodhabits/ui/calendar/CalendarFragment.java
+++ app/src/main/java/moe/haruue/goodhabits/ui/calendar/CalendarFragment.java
@@ -2,9 +2,16 @@ package moe.haruue.goodhabits.ui.calendar;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import moe.haruue.goodhabits.R;
 import moe.haruue.goodhabits.ui.BaseFragment;
@@ -25,6 +32,30 @@ public class CalendarFragment extends BaseFragment implements CalendarContract.V
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_calendar, container, false);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView textView = (TextView) view.findViewById(R.id.tv_calendar_title);
+        Calendar calendar = Calendar.getInstance();
+        textView.setText(calendar.get(GregorianCalendar.MONTH)+"月"+
+                calendar.get(GregorianCalendar.DAY_OF_MONTH)+"日");
+
+        // TODO: 2016/8/22 theckout data
+        ArrayList<Boolean> arrayList = new ArrayList<>();
+        for (int i = 0; i < 42; i++) {
+            if (i % 2 == 0) {
+                arrayList.add(i, true);
+            } else {
+                arrayList.add(i, false);
+            }
+        }
+
+        RecyclerView rv = (RecyclerView) view.findViewById(R.id.rv_calendar);
+        rv.setLayoutManager(new GridLayoutManager(getContext(), 7));
+        rv.setAdapter(new CalendarAdapter(arrayList, getContext()));
     }
 
     @Override
