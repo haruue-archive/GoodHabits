2c905da5fddc2173261f461885734ab1316af9f8
Haruue Icymoon <haruue@caoyue.com.cn>
2016-08-23 17:40:39 +0800

    实现 CalendarPresneter#getFinishedPer 和 TaskPresenter#saveNote

--- app/src/main/java/moe/haruue/goodhabits/ui/calendar/CalendarPresneter.java
+++ app/src/main/java/moe/haruue/goodhabits/ui/calendar/CalendarPresneter.java
@@ -1,5 +1,17 @@
 package moe.haruue.goodhabits.ui.calendar;
 
+import android.util.Log;
+
+import java.util.GregorianCalendar;
+
+import moe.haruue.goodhabits.data.database.task.func.TasksByTimeQueryFunc;
+import moe.haruue.goodhabits.model.Task;
+import moe.haruue.goodhabits.util.TimeUtils;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
 /**
  * Created by simonla on 2016/8/18.
  * Have a good day.
@@ -15,6 +27,42 @@ public class CalendarPresneter implements CalendarContract.Presenter {
 
     @Override
     public void start() {
+
+    }
+
+    @Override
+    public void getFinishedPer(CalendarContract.Callback callback) {
+        Observable.just(Task.newEmptyTaskWithStartTimeAndEndTime(0, TimeUtils.getTimeStampOf(new GregorianCalendar())))
+                .map(new TasksByTimeQueryFunc())
+                .map(tasks -> {
+                    int finishedTastCount = 0;
+                    for (Task t: tasks) {
+                        if (t.isFinish) {
+                            finishedTastCount++;
+                        }
+                    }
+                    return (int) (((double) finishedTastCount) / ((double) tasks.size()) * 100);
+                })
+                .subscribeOn(Schedulers.io())
+                .unsubscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e("CalendarPresneter", "getFinishPer", e);
+                        callback.onError(e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        callback.onFinish(integer);
+                    }
+                });
 
     }
 }
