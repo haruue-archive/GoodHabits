5ac8638e6a845719007cc665cb4549100951d910
simonla <zengmingyang96@gmail.com>
2016-08-25 19:12:57 +0800

    .

--- app/src/main/java/moe/haruue/goodhabits/ui/square/SquarePresenter.java
+++ app/src/main/java/moe/haruue/goodhabits/ui/square/SquarePresenter.java
@@ -1,5 +1,9 @@
 package moe.haruue.goodhabits.ui.square;
 
+import java.util.ArrayList;
+
+import moe.haruue.goodhabits.model.Plan;
+
 /**
  * Created by simonla on 2016/8/18.
  * Have a good day.
@@ -18,4 +22,21 @@ public class SquarePresenter implements SquareContract.Presenter {
 
     }
 
+/*    @Override
+    public void getPlans(SquareContract.Callback callback) {
+        ArrayList<Plan> arrayList = new ArrayList<>();
+        Plan plan = new Plan();
+        plan.title = "感觉更有活力";
+        arrayList.add(plan);
+        Plan plan1 = new Plan();
+        plan1.title = "瘦成一道闪电";
+        arrayList.add(plan1);
+        Plan plan2 = new Plan();
+        plan2.title = "改善睡眠质量";
+        arrayList.add(plan2);
+        Plan plan3 = new Plan();
+        plan3.title = "提高专注力";
+        arrayList.add(plan3);
+        callback.onSuccess(arrayList);
+    }*/
 }
