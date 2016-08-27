034c5214d9c2beaee061a560a73e1867ee7be1d4
Haruue Icymoon <haruue@caoyue.com.cn>
2016-08-27 21:51:36 +0800

    查询当天 Tasks

--- app/src/main/java/moe/haruue/goodhabits/receiver/AlarmReceiver.java
+++ app/src/main/java/moe/haruue/goodhabits/receiver/AlarmReceiver.java
@@ -6,6 +6,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import moe.haruue.goodhabits.data.database.task.func.TasksByTimeQueryFunc;
+import moe.haruue.goodhabits.model.Task;
+import moe.haruue.goodhabits.util.TimeUtils;
+
 /**
  * Created by simonla on 2016/8/27.
  * Have a good day.
@@ -19,6 +26,8 @@ public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         //需要List<Task> mTasks
+        long todayStartTimeStamp = TimeUtils.timeStampToDayStart(new GregorianCalendar().getTimeInMillis() / 1000);
+        List<Task> mTasks = new TasksByTimeQueryFunc().call(Task.newEmptyTaskWithStartTimeAndEndTime(todayStartTimeStamp, todayStartTimeStamp + 86400));
         if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
             /*mAlarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
             Intent intent1 = new Intent(context, PushReceiver.class);
